9dbf73a315bb3e34083386b6c0b0635b3a88367c
Adam Retter <adam.retter@googlemail.com>
2023-10-12 22:59:45 +0200

    Install pyenv and update Python to 3.12.0

--- site-modules/profile/manifests/docker.pp
+++ site-modules/profile/manifests/docker.pp
@@ -1,5 +1,5 @@
-class profile::docker {
-  include profile::python
+class profile::docker (String $pyenv_python_version = '3.12.0', String $pyenv_root = '/home/ec2-user/.pyenv') {
+  require profile::python
 
   package { 'docker':
     ensure => installed,
@@ -24,9 +24,18 @@ class profile::docker {
     ],
   }
 
-  package { 'docker-compose':
-    ensure   => installed,
-    provider => 'pip3',
-    require  => Package['python3-pip'],
+  # TODO(AR) not sure which pip3 this will find so just use the explicit pip command below
+  # package { 'docker-compose':
+  #   ensure   => installed,
+  #   provider => 'pip3',
+  #   require  => Package['python3-pip'],
+  # }
+
+  exec { 'pip-install-docker-compose':
+    command     => "${pyenv_root}/shims/pip install docker-compose",
+    environment => ["PYENV_ROOT=${pyenv_root}"],
+    unless      => "${pyenv_root}/shims/pip show docker-compose",
+    user        => 'ec2-user',
+    require     => Exec["pyenv-install-python-${pyenv_python_version}"],
   }
 }
