1e47a0a7e6609ed413a8be44b098df784aa9f865
Adam Retter <adam.retter@googlemail.com>
2023-10-18 10:07:21 +0200

    Decompose the 'sql_x_development' module

--- site-modules/profile/manifests/sql_x_development.pp
+++ site-modules/profile/manifests/sql_x_development.pp
@@ -1,31 +1,3 @@
 class profile::sql_x_development {
-  include profile::x_desktop
-  include yum
-
-  yum::install { 'azuredatastudio':
-    ensure => present,
-    source => 'https://sqlopsbuilds.azureedge.net/stable/65fb22cc7c36db9c53af1ed2fdbdf48f66c682be/azuredatastudio-linux-1.31.1.rpm',
-  }
-
-  file { '/home/ec2-user/Desktop/AzureDataStudio.desktop':
-    ensure  => file,
-    owner   => 'ec2-user',
-    group   => 'ec2-user',
-    mode    => '0766',
-    content => "[Desktop Entry]
-Version=1.0
-Type=Application
-Name=Azure Data Studio
-Exec=/usr/bin/azuredatastudio
-Icon=/usr/share/azuredatastudio/resources/app/resources/linux/code.png
-Terminal=false
-StartupNotify=false
-GenericName=Microsoft Azure Data Studio
-",
-    require => [
-      Yum::Group['Xfce'],
-      Yum::Install['azuredatastudio'],
-      File['/home/ec2-user/Desktop']
-    ],
-  }
+  include profile::azuredatastudio_x
 }
