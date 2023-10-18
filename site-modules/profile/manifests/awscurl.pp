d4e8c3a88a239cae5216871ddb9821d8a5a3552b
Adam Retter <adam.retter@googlemail.com>
2023-10-18 21:01:55 +0200

    Use same default version of Python (3.12.0) and the same default user for awscurl

--- site-modules/profile/manifests/awscurl.pp
+++ site-modules/profile/manifests/awscurl.pp
@@ -1,11 +1,11 @@
-class profile::awscurl (String $pyenv_python_version = '3.12.0', String $pyenv_root = '/home/ec2-user/.pyenv') {
+class profile::awscurl (String $pyenv_python_version = '3.12.0', String $pyenv_root = '/home/ec2-user/.pyenv', String $user = 'ec2-user') {
   require profile::python
 
   exec { 'pip-install-awscurl':
     command     => "${pyenv_root}/shims/pip install awscurl",
     environment => ["PYENV_ROOT=${pyenv_root}"],
     unless      => "${pyenv_root}/shims/pip show awscurl",
-    user        => 'ec2-user',
+    user        => $user,
     require     => Exec["pyenv-install-python-${pyenv_python_version}"],
   }
 }
